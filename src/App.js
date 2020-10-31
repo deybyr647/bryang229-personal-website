6d7bdb268d4554260498b7ac22fc66601c3b9933
deybyr647 <deybyr647@gmail.com>
2020-10-31 02:49:01 -0400

    Completed landing page, removed unnecessary files and added some images

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
-import {Nav, Navbar} from 'react-bootstrap';
+import {Nav, Navbar, Image} from 'react-bootstrap';
 
 import Home from './pages/home';
+
+import logo from './images/Logo.svg'
 
 const Navigation = () => (
   <>
   <Navbar variant='light' expand="lg">
-    <Navbar.Brand as={Link} to='/'>Bryan Bonilla Garay</Navbar.Brand>
+    <Navbar.Brand as={Link} to='/'><Image width="50px" src={logo}/></Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="font-weight-bold">
+      <Nav className="heading font-weight-bold">
         <Nav.Link as={Link} to='/about'>About</Nav.Link>
         <Nav.Link as={Link} to='/portfolio'>Portfolio</Nav.Link>
         <Nav.Link as={Link} to='/resume'>Resume</Nav.Link>
@@ -27,9 +29,18 @@ const Navigation = () => (
   </>
 )
 
+const Footer = () => (
+  <Nav className='sticky-bottom justify-content-center'>
+      <Nav.Link className='text-body heading font-weight-bold' href='https://deybyr647.com' target='_blank' rel='noopener noreferrer'>
+        &copy; 2020 | Deyby Rodriguez
+      </Nav.Link>
+  </Nav>
+)
+
 const App = () => (
   <Router>
     <Navigation />
+    <Footer/>
   </Router>
 )
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,20 +1,62 @@
 import './home.css';
-import {Row, Col, Container, Jumbotron} from 'react-bootstrap';
+import facePic from '../../images/bryan.jpg';
+import hw from '../../images/hw.jpg';
+
+import {Row, Col, Container, Jumbotron, Image, Card, CardDeck} from 'react-bootstrap';
 
 const ImageJumbotron = () => (
     <Row>
-        <Col className='mt-5'>
-            <Jumbotron className='m-auto text-center'>
+        <Col xs={10} sm={5} md={8} lg={6} xl={5} className='mt-4 mx-auto'>
+            <Jumbotron className='m-auto text-center shadow'>
+                <Image src={facePic} fluid rounded className='mb-5 shadow-lg'/>
                 <h3>Bryan Bonilla Garay</h3>
             </Jumbotron>
         </Col>
     </Row>
-)
+);
+
+const LandingCard = ({obj}) => (
+    <Card className='shadow text-center mx-4 mt-4'>
+        <Card.Img variant='top' src={obj.image}/>
+        <Card.Body>
+            <Card.Title>{obj.title}</Card.Title>
+            <Card.Text>
+                {obj.description}
+            </Card.Text>
+        </Card.Body>
+    </Card>
+);
+
+const cardContent = [
+    {
+        title: "Hardware",
+        image: hw,
+        description: "Lorem ipsum dolor sit amet, consectetur adispising elit",
+    },
+    {
+        title: "A Bit About Me...",
+        image: null,
+        description: "Lorem ipsum dolor sit amet, consectetur adispising elit. Lorem ipsum dolor sit amet, consectetur adispising elit. Lorem ipsum dolor sit amet, consectetur adispising elit"
+    }
+]
+
+const CardsArea = ({children}) => (
+    <Row className='my-3 mx-auto'>
+        <Col xs={12} md={12} lg={12} xl={10} className='my-3 mx-auto'>
+            <CardDeck>
+                {children}
+            </CardDeck>
+        </Col>
+    </Row>
+);
 
 const Home = () => (
     <Container>
         <ImageJumbotron/>
+        <CardsArea>
+            {cardContent.map(el => <LandingCard obj={el}/>)}
+        </CardsArea>
     </Container>
-)
+);
 
 export default Home;
